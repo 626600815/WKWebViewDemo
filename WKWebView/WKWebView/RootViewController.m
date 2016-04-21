38da7f1042341ad95e94b2dc9064636c7b0d05c3
meteor_1990 <626600815@qq.com>
2016-04-21 10:06:13 +0800

    部分调整

--- WKWebView/WKWebView/RootViewController.m
+++ WKWebView/WKWebView/RootViewController.m
@@ -26,6 +26,12 @@
         [self.navigationController presentViewController:safari animated:YES completion:nil];
     }else {
         NSLog(@"该升级系统啦,骚年!");
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"警告框" message:@"该升级系统啦,骚年!" preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+            [alert dismissViewControllerAnimated:YES completion:nil];
+        }];
+        [alert addAction:okAction];
+        [self presentViewController:alert animated:YES completion:nil];
     }
     
 }
